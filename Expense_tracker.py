559b97f230b486603dd8638e9b381213e22feecf
Pawan Kumar Yadav <sspawanky@gmail.com>
2026-04-14 12:02:37 +0530

    Creating Menu

---
+++ Expense_tracker.py
@@ -0,0 +1,12 @@
+#Expense Tracker Project
+
+expenses = [] #list of all expenses
+
+while True:
+    print("===MENU===")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Total Kharcha")
+    print("4. Exit")
+
+    
