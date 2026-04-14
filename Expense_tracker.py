aaf5174929a898943b503da666439d71a238789c
Pawan Kumar Yadav <sspawanky@gmail.com>
2026-04-14 12:21:19 +0530

    Adding choice 1

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -9,4 +9,20 @@ while True:
     print("3. Total Kharcha")
     print("4. Exit")
 
-    
+    choice=int(input("Enter your choices:"))
+    if(choice==1):
+        date=input("Enter the date of the expense (dd/mm/yyyy): ")
+        category=input("Enter the category of the expense (eg: food, transport,books,etc..): ")
+        description=input("Enter the description of the expense: ")
+        amount=float(input("Enter the amount spent: "))
+
+        expense={
+            "date": date,
+            "category": category,
+            "description": description,
+            "amount": amount
+        }
+        expenses.append(expense)
+        print("Expense added successfully !\n")
+
+        
